345343e2a9c3172634aac9d4bc516dc223cf4341
Arpit <alfabatforarpitjain@gmail.com>
2022-12-07 00:27:19 +0530

    fix manager login issue.

--- app/src/main/java/aatmanirbharbharat/app/atmanirbharops/util/Constant.kt
+++ app/src/main/java/aatmanirbharbharat/app/atmanirbharops/util/Constant.kt
@@ -4,7 +4,8 @@ interface Constant {
     companion object {
         const val SHARED_PREF = "sharedpp"
         const val TOKEN = "token"
-        const val BASE_URL = "https://assleit.com/atmanirbar_bharat/"
+      //  const val BASE_URL = "https://assleit.com/atmanirbar_bharat/"
+        const val BASE_URL = "https://aamjan.co.in/"
         const val LOGINAPI = "api/manager/login/authenticate"
     }
 }
